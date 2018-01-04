900d20998f59f35ec6c00615c40ebb890bf7c2f7
David White <david.white@deltadna.com>
2018-01-04 17:08:08 +0000

    Created a test stub.

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -1,47 +1,26 @@
 //
-//  DeltaDNANotificationsTests.m
-//  DeltaDNANotificationsTests
+// Copyright (c) 2017 deltaDNA Ltd. All rights reserved.
 //
-//  Created by dwxw on 11/17/2017.
-//  Copyright (c) 2017 dwxw. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 
-// https://github.com/Specta/Specta
+#import <Specta/Specta.h>
+#import <Expecta/Expecta.h>
 
 SpecBegin(InitialSpecs)
 
-describe(@"these will fail", ^{
-
-    it(@"can do maths", ^{
-        expect(1).to.equal(2);
-    });
-
-    it(@"can read", ^{
-        expect(@"number").to.equal(@"string");
-    });
-    
-    it(@"will wait for 10 seconds and fail", ^{
-        waitUntil(^(DoneCallback done) {
-        
-        });
-    });
-});
-
-describe(@"these will pass", ^{
-    
-    it(@"can do maths", ^{
-        expect(1).beLessThan(23);
-    });
-    
-    it(@"can read", ^{
-        expect(@"team").toNot.contain(@"I");
-    });
-    
-    it(@"will wait and succeed", ^{
-        waitUntil(^(DoneCallback done) {
-            done();
-        });
-    });
+describe(@"test", ^{
+    expect(1).to.beGreaterThan(0);
 });
 
 SpecEnd
